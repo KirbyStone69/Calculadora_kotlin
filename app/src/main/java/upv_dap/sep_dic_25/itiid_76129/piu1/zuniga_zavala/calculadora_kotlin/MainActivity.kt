1c374f7d97a2b282ee392f278e57798e0560b500
KirbyStone69 <2430206@upv.edu.mx>
2025-09-06 01:57:21 -0600

    Boton funcional

--- app/src/main/java/upv_dap/sep_dic_25/itiid_76129/piu1/zuniga_zavala/calculadora_kotlin/MainActivity.kt
+++ app/src/main/java/upv_dap/sep_dic_25/itiid_76129/piu1/zuniga_zavala/calculadora_kotlin/MainActivity.kt
@@ -1,9 +1,7 @@
 package upv_dap.sep_dic_25.itiid_76129.piu1.zuniga_zavala.calculadora_kotlin
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var TextView1: TextView
     lateinit var Boton1: Button
     lateinit var EditText1: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,22 +22,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        TextView1 = findViewById<TextView>(R.id.edit)
+        TextView1 = findViewById<TextView>(R.id.nombre)
         Boton1 = findViewById<Button>(R.id.Button1)
-        EditText1 = findViewById<EditText>(R.id.nombre)
+        EditText1 = findViewById<EditText>(R.id.edit)
 
-        Boton1!!.setOnClickListener(object : View.OnClickListener {
-            override fun onClick (v : View?){
-                toast.makeText(
-                    getApplicationContext(),
-                    "Bienvenido señor" + EditText1.getText().toString(),
-                    toast.LENGTH_SHORT
-                ).show()
-                TextView1.setText(EditText1.getText().toString())
-            }
-        })
+        Boton1.setOnClickListener {
+            Toast.makeText(
+                applicationContext,
+                "Bienvenido don "+ EditText1.text.toString(),
+                Toast.LENGTH_SHORT
+            ).show()
+            TextView1.text = EditText1.text.toString()
+        }
 
 
         }
     }
-}
